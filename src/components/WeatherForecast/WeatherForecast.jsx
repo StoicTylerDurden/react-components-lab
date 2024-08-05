806216936dbc4b5493b3f5ed9a1cac8f05aa2fa7
StoicTylerDurden <buhussayyen@gmail.com>
2024-08-05 15:09:26 +0300

    Finished WeatherForecast exercise

---
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -0,0 +1,14 @@
+import './WeatherForecast.css'
+const WeatherForecast = ({weather}) => {
+    const {day, img, imgAlt, conditions, time} = weather;
+    return (
+        <div className="weather">
+            <h2>Day of the Week {day}</h2>
+            <img src={img} alt={imgAlt} />
+            <p><span>conditions: </span>{conditions}</p>
+            <p><span>time: </span>{time}</p>
+        </div>
+    )
+}
+export default WeatherForecast;
+
